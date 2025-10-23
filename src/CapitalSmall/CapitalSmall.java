457de869447b08d0a296f5b01bccea97ac37fd8a
prantonub <prantonub.cse23@gmail.com>
2025-10-23 16:47:11 +0600

    task added

---
+++ src/CapitalSmall/CapitalSmall.java
@@ -0,0 +1,22 @@
+package CapitalSmall;
+
+import java.util.Scanner;
+
+public class CapitalSmall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        char ch;
+        System.out.print("Enter any letter: ");
+        ch = input.next().charAt(0);
+
+        if(ch>='a' && ch<='z'){
+            System.out.println("Small Letter");
+        }
+        else if(ch>='A' && ch<='Z'){
+            System.out.println("Capital Letter");
+        }
+        else{
+            System.out.println("Not a Letter");
+        }
+    }
+}
