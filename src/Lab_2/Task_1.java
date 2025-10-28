6b35f579405e4b016e8e5933f200f99f0a8881a6
prantonub <prantonub.cse23@gmail.com>
2025-10-28 20:10:55 +0600

    task added

---
+++ src/Lab_2/Task_1.java
@@ -0,0 +1,17 @@
+package Lab_2;
+
+public class Task_1 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 3, 4, 4, 5};
+
+        for (int i = 0; i < arr.length; i++) {
+            int count = 0;
+            for (int j = 0; j < arr.length; j++) {
+                if (arr[i] == arr[j])
+                    count++;
+            }
+            if (count == 1)
+                System.out.println(arr[i]);
+        }
+    }
+}
