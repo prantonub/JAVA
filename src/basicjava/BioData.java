d1bade3889c73a24d5fa028116384ae17e522235
prantonub <prantonub.cse23@gmail.com>
2025-10-30 22:08:08 +0600

    task added

--- src/basicjava/BioData.java
+++ src/basicjava/BioData.java
@@ -5,6 +5,7 @@ public class BioData {
     System.out.println("Tauhidul Islam Pranto");
     System.out.println("Northern University Bangladesh");
     System.out.println("Department of CSE");
+    System.out.println("Dhaka, Bangladesh");
     /*All in just one line code:
     System.out.println(" Tauhidul Islam Pranto \n Northern University Bangladesh \n Department of CSE ");
     */
