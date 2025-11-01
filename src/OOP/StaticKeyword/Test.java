7d06c24b40b2ee085e1d038ca9ef21a79763a3d0
prantonub <prantonub.cse23@gmail.com>
2025-11-01 22:06:13 +0600

    task added

---
+++ src/OOP/StaticKeyword/Test.java
@@ -0,0 +1,11 @@
+package OOP.StaticKeyword;
+
+public class Test {
+    public static void main(String[] args) {
+        Student s1 = new Student("Pranto", 1217);
+        Student s2= new Student("Minhaz", 1227);
+
+        s1.displayInformation();
+        s2.displayInformation();
+    }
+}
