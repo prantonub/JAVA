3e4a4fe102fb8c0ea6a943a1af7fb17951a27560
prantonub <prantonub.cse23@gmail.com>
2025-11-03 09:49:31 +0600

    task added

---
+++ src/OOP/MethodOverloading/Test.java
@@ -0,0 +1,12 @@
+package OOP.MethodOverloading;
+
+public class Test {
+public static void main(String[] args) {
+        MethodOverloading ob1 = new MethodOverloading();
+
+    ob1.add(5,10);
+    ob1.add(5.2, 5.8);
+    ob1.add(5,10, 9);
+    ob1.add();
+}
+}
