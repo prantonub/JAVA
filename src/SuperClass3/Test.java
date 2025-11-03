0b934e2ab4c108c61b0697e9b03035702a44d8aa
prantonub <prantonub.cse23@gmail.com>
2025-11-04 05:44:02 +0600

    task added

---
+++ src/SuperClass3/Test.java
@@ -0,0 +1,13 @@
+package SuperClass3;
+
+public class Test {
+    public static void main(String[] args) {
+        Car c1 = new Car("RED", 350, 5);
+    c1.attribute();
+
+System.out.println();//only using for new line
+
+    Car c2 = new Car("Green", 550, 20);
+    c2.attribute();
+    }
+}
